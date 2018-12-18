23fdbd9127ffe9e83835b2633833978c14db77c1
Viet Hoa Dinh <dvh@fb.com>
2018-12-17 18:09:18 -0800

    Fixed separator in dark mode

--- Sources/UI/appkit+djl/DJLGradientSeparatorLineView.m
+++ Sources/UI/appkit+djl/DJLGradientSeparatorLineView.m
@@ -3,12 +3,19 @@
 
 #import "DJLGradientSeparatorLineView.h"
 
+#import "DJLDarkMode.h"
+
 @implementation DJLGradientSeparatorLineView
 
 - (void)drawRect:(NSRect)dirtyRect
 {
     NSColor * leftColor = [NSColor clearColor];
-    NSColor * middleColor = [NSColor colorWithCalibratedWhite:0.0 alpha:0.1];
+    NSColor * middleColor;
+    if ([DJLDarkMode isDarkModeForView:self]) {
+        middleColor = [NSColor colorWithCalibratedWhite:1.0 alpha:0.1];
+    } else {
+        middleColor = [NSColor colorWithCalibratedWhite:0.0 alpha:0.1];
+    }
     NSColor * rightColor = [NSColor clearColor];
     NSGradient * gradient = [[NSGradient alloc] initWithColorsAndLocations:leftColor, (CGFloat) 0.0, middleColor, (CGFloat) 0.2, middleColor, (CGFloat) 0.8, rightColor, (CGFloat) 1.0, nil];
     NSRect rect = [self bounds];
